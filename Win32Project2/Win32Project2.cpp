8c5a7966e0857998e4fd013059372a90dd541232
Luke <Luke@PULSEDRIVE>
2016-04-21 18:57:31 +0100

    Add project files.

---
+++ Win32Project2/Win32Project2.cpp
@@ -0,0 +1,76 @@
+#include "stdafx.h"
+#include <iostream>
+
+
+using namespace std;
+
+/*int main()
+{
+	int a;
+	cout << "Please enter a number: ";
+	cin >> a;
+	int b;
+	cout << "Please enter a second number: ";
+	cin >> b; 
+	int result;
+	result = a + b;
+	cout << "By adding Number 1:" << a <<" "<< "and Number 2:" << b << " " << "The Result Is: " << result << "\n";
+
+	string s;
+	cout << "Please Input Your Name ";
+	cin >> s;
+	cout << "Thanks For Your Input " << "\"" << s << "\"" << "\n";
+	
+	system("Pause");
+	return 0;
+} 
+
+int main()
+{
+	int mark;
+	cout << "Please Input Students Mark: ";
+	cin >> mark;
+	string name;
+	cout << "Please Input Student's Name: ";
+	cin >> name;
+
+	if (mark < 50) {
+		cout << name << " ";
+		cout << "has failed with a mark of " << mark << endl;
+		cout << "Sorry" << endl;
+	}
+	else {
+		cout << name << " ";
+		cout << "Has Passed with a mark of " << mark << endl;
+		cout << "Congratulations!" << endl;
+	}
+	system("pause");
+	return 0;
+} */
+
+/*int main()
+{
+
+	int numberOne;
+	int numberTwo;
+	int result;
+
+	cout << "Please enter your first number: ";
+	cin >> numberOne;
+	cout << "Please enter your second number: ";
+	cin >> numberTwo;
+
+	if (numberOne > numberTwo) {
+		cout << "Number one is greater than number two" << endl;
+	}
+	if (numberOne == numberTwo) {
+		cout << "Number one and number two are equal" << endl;
+	}
+	if (numberOne < numberTwo) {
+		cout << "Number two is greater than number one" << endl;
+	}
+
+	system("Pause");
+	return 0;
+} */
+
